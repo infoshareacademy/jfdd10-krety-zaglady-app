6dbfc12b65568d70c93ee6ad6252836566988208
AleksandraNowicka <aleksandranowickajasztal@gmail.com>
2018-10-11 15:42:21 +0200

    First Component

---
+++ src/components/ProjectAuthor/ProjectAuthor.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class ProjectAuthor extends Component {
+
+
+
+  
+
+  render() {
+    return (
+     <div> hello</div> 
+    )
+  }
+}
+
+export default ProjectAuthor
---
+++ src/components/ProjectAuthor/ProjectAuthor.md
@@ -0,0 +1,5 @@
+Example  ProjectAuthor:
+
+```js
+<ProjectAuthor  />
+```
