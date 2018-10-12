0c9882e8b20aefde97a3924ffbfd87c71aeaab87
Balbuhhha <balbuhhha@gmail.com>
2018-10-12 15:30:10 +0200

    files created

---
+++ src/components/ProjectList/ProjectList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './ProjectList.css'
+
+class ProjectList extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return  (
+      <div className="ProjectList">
+        ProjectList
+      </div>
+    )
+  }
+}
+
+export default ProjectList
