028d6eeda288e19951af740c101348a21cf4adaa
Balbuhhha <balbuhhha@gmail.com>
2018-10-15 16:04:58 +0200

    Display with no projects

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -4,12 +4,11 @@ import ProjectListItem from "../ProjectListItem";
 import "./ProjectList.css";
 
 class ProjectList extends Component {
-
   // showHeadline = () => {
   //   if (this.props.projects === []) {
   //     <span>There are no projects yet</span>
   //     }
-  //     <span>Projects</span>
+  //     return
   //   }
 
   // }
@@ -17,12 +16,21 @@ class ProjectList extends Component {
   render() {
     return (
       <div className="ProjectList">
-        <span>Projects</span>
-        {this.props.projects.map(project => (
-          <div class="ProjectList-projectListItem" key={project.id}>
-            <ProjectListItem {...project} />
-          </div>
-        ))}
+        {this.props.projects.length !== 0 ? (
+          <>
+            <span>Projects</span>
+            {this.props.projects.map(project => (
+              <div class="ProjectList-projectListItem" key={project.id}>
+                <ProjectListItem {...project} />
+              </div>
+            ))}
+          </>
+        ) : (
+          <>
+          <span>There are no projects yet</span>
+          <img class="ProjectList-sadPanda" src="https://cdn1.iconfinder.com/data/icons/panda-emojis-2/64/Sad_panda_face_bear_copy-512.png" alt="sad panda" />
+          </>
+        )}
       </div>
     );
   }
