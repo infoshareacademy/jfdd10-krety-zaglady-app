5075f5518db8bfc6d3d40535786c07d32e5e2c53
Balbuhhha <balbuhhha@gmail.com>
2018-10-16 17:10:12 +0200

    project.json created

---
+++ src/components/App/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import HomeView from "../HomeView";
+import ProjectView from "../ProjectView";
+// import UserProfileView from './components/UserProfileView'
+
+import "./App.css";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/project">My Project</NavLink>
+              </li>
+              <li>
+                <NavLink to="/user">My Profile</NavLink>
+              </li>
+            </ul>
+
+            <Route exact path="/" component={HomeView} />
+            <Route path="/project" component={ProjectView} />
+            {/* <Route exact path="/user" component={UserProfileView} /> */}
+          </header>
+        </div>
+      </Router>
+    );
+  }
+}
+export default App;
--- src/components/ProjectView/ProjectView.js
+++ src/components/ProjectView/ProjectView.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import "./ProjectView.css";
+import ProjectList from "../ProjectList";
 class ProjectView extends Component {
   render() {
     return (
-      <div className="main_box"> 
-      
+      <div className="main_box">
+        <ProjectList projects={[]} />
         <div className="top_box" />
       </div>
     );
