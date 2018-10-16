ff285df6b52d79c9b3c21f7cbc16a5236310e71a
Balbuhhha <balbuhhha@gmail.com>
2018-10-16 17:17:01 +0200

    Bartosz helped with props

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,41 +1,13 @@
 import React, { Component } from "react";
-import ProjectList from "../ProjectList"
-import ProjectAuthor from '../ProjectAuthor/ProjectAuthor'
+import ProjectList from "../ProjectList";
+import ProjectAuthor from "../ProjectAuthor/ProjectAuthor";
 
 import "./HomeView.css";
 class HomeView extends Component {
   render() {
     return (
-      <div className="HomeView-main_box"> 
-        <ProjectAuthor
-  userName="Stefan"
-  userSurname="Kowalski"
-  userImage={'https://cdn3.iconfinder.com/data/icons/avatar-55/64/Gardener-avatar-occupation-profession-woman-human-512.png'}
-  fruits={[
-  {
-    image: 'https://cdn3.iconfinder.com/data/icons/spring-23/32/carrot-vegetable-spring-food-512.png',
-    alt: 'carrot'
-  },
-  {
-    image: 'https://cdn3.iconfinder.com/data/icons/fruits-8/512/apple-512.png',
-    alt: 'apple'
-      },
-  {
-    image: 'https://cdn3.iconfinder.com/data/icons/fruits-52/150/icon_fruit_cerejas-512.png',
-    alt: 'cherry'
-  },
-  {
-    image: 'https://cdn3.iconfinder.com/data/icons/fruits-52/150/icon_fruit_ameixa-512.png',
-    alt: 'plum'
-  },
-  {
-    image: 'https://cdn3.iconfinder.com/data/icons/veggies/512/tomato.png',
-    alt: 'tomato'
-  }
-    ]}
-  description="I like gardening and my dream is to grow my own vegetables"
-  boardImage={"https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678074-map-512.png"}
-  />
+      <div className="HomeView-main_box">
+        <ProjectAuthor {...this.props.author} />
         <div className="HomeView-top_box" />
         <ProjectList projects={[]} />
       </div>
--- src/components/HomeView/HomeView.md
+++ src/components/HomeView/HomeView.md
@@ -1,3 +1,3 @@
 ```js
-<HomeView />
+<HomeView author={{ }}/>
 ```
