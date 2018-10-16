bf3bb4f9b4511da308f4a4578881a63f529c7a3e
AleksandraNowicka <aleksandranowickajasztal@gmail.com>
2018-10-16 17:00:16 +0200

    Change HomeView

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import ProjectList from "../ProjectList"
+import ProjectAuthor from '../ProjectAuthor/ProjectAuthor'
+
+import "./HomeView.css";
+class HomeView extends Component {
+  render() {
+    return (
+      <div className="HomeView-main_box"> 
+        <ProjectAuthor></ProjectAuthor>
+        <div className="HomeView-top_box" />
+        <ProjectList projects={[]} />
+      </div>
+    );
+  }
+}
+export default HomeView;
