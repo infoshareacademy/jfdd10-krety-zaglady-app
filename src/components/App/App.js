11d4613d2304e33dd76a39b20f7c5030465faac8
Lukasz <rgpszczecin@gmail.com>
2018-10-19 16:22:19 +0200

    Refreshing view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,30 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import HomeView from "../HomeView";
 import ProjectView from "../ProjectView";
 import UserProfileView from "../UserProfileView";
-
+import posed from "react-pose";
 import "./App.css";
 import ProjectAuthor from "../ProjectAuthor/ProjectAuthor";
+const PushUp = posed.div({
+  idle: { scale: 1 },
+  hovered: { scale: 1.5 }
+});
 class App extends Component {
+
+  state = { hovering: false };
+
   render() {
     return (
       <Router>
         <div className="App">
           <header className="App-header">
             <NavLink exact to="/">
+            <PushUp
+            pose={this.state.hovering ? "hovered" : "idle"}
+            onMouseEnter={() => this.setState({ hovering: true })}
+            onMouseLeave={() => this.setState({ hovering: false })}
+          >
               <img src="/data/molelogo.png" alt="home" className="App-logo" />
+            </PushUp>
             </NavLink>
 
             <Route exact path="/" component={HomeView} />
