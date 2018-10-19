f49858d534fcf049390a9b338880a9ba55df1c5f
Balbuhhha <balbuhhha@gmail.com>
2018-10-19 16:04:51 +0200

    links work as they should in HomeView

--- src/components/ProjectAuthor/ProjectAuthor.js
+++ src/components/ProjectAuthor/ProjectAuthor.js
@@ -4,11 +4,12 @@ import { Link } from "react-router-dom";
 class ProjectAuthor extends Component {
   render() {
     return (
-      <Link
+      
+        <div className="ProjectAuthor">
+        <Link
         to={"/projects/user/" + this.props.id}
         className="ProjectAuthorLink"
       >
-        <div className="ProjectAuthor">
           <h1 className="ProjectAuthor-header">{this.props.name}</h1>
           <div className="ProjectListItem-userInfo">
             <img src={this.props.userImage} alt="userImage" />
@@ -21,8 +22,9 @@ class ProjectAuthor extends Component {
           </div>
           {/* <p className="ProjectAuthor-photo">{this.props.photo}</p> */}
           {/* <img src="https://via.placeholder.com/350x150" /> */}
+          </Link>
         </div>
-      </Link>
+     
     );
   }
 }
