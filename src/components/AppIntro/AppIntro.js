10890641f1f61bfda85473d36cb7b5e53cfc071e
AleksandraNowicka <aleksandranowickajasztal@gmail.com>
2018-10-25 15:59:24 +0200

    Add new component AppIntro

---
+++ src/components/AppIntro/AppIntro.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "./AppIntro.css" ;
+
+
+
+class AppIntro extends Component {
+  
+
+render() {
+    return (
+      
+        <div className="AppIntro">
+        <div className="MeloIintro"></div>
+        <div className="Intro"><h1> WITAJCIE W KRETOGRODZIE! </h1></div>
+     
+       
+        </div>
+        
+      
+    );
+  }
+};
+export default AppIntro;
