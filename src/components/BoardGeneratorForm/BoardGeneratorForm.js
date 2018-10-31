0971b31b1d1f7a3defee9023501b948b1d869d37
Balbuhhha <balbuhhha@gmail.com>
2018-10-31 14:25:14 +0100

    handlers for checkboxes work

--- src/components/BoardGeneratorForm/BoardGeneratorForm.js
+++ src/components/BoardGeneratorForm/BoardGeneratorForm.js
@@ -39,12 +39,16 @@ class BoardGeneratorForm extends Component {
   };
 
 
-  handleChange = (event) => (
+  
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
     this.setState({
-      [event.target.name]: event.target.value
-    })
-  )
- 
+      [name]: value
+    });
+  }
 
   render() {
     return (
@@ -61,7 +65,7 @@ class BoardGeneratorForm extends Component {
                 name="userDescriptionTitle"
                 placeholder="Tytuł projektu..."
                 value={this.state.userDescriptionTitle}
-                onChange={this.handleChange}
+                onChange={this.handleInputChange}
               />
             </label>
             <label>
@@ -73,7 +77,7 @@ class BoardGeneratorForm extends Component {
                 cols="30"
                 placeholder="Opis projektu..."
                 value={this.state.userGardenDescription}
-                onChange={this.handleChange}
+                onChange={this.handleInputChange}
               />
             </label>
             <h3>Wielkość ogródka</h3>
@@ -86,7 +90,7 @@ class BoardGeneratorForm extends Component {
                 max="10"
                 name="size"
                 value={this.state.size}
-                onChange={this.handleChange}
+                onChange={this.handleInputChange}
               />
             </label>
             <span>{this.state.size} rzędów i kolumn</span>
@@ -100,43 +104,47 @@ class BoardGeneratorForm extends Component {
             <div className="BoardGeneratorForm-fruitContainer">
               <label name="cherry">
                 <img src="https://cdn3.iconfinder.com/data/icons/fruits-52/150/icon_fruit_cerejas-512.png" />
-                <input type="checkbox" name="cherry" />
+                <input type="checkbox" name="cherry" onChange={this.handleInputChange}/>
               </label>
               <label name="apple">
                 <img src="https://cdn3.iconfinder.com/data/icons/fruits-8/512/apple-512.png" />
-                <input type="checkbox" name="apple" />
+                <input type="checkbox" name="apple" onChange={this.handleInputChange}/>
               </label>
               <label name="plum">
                 <img src="https://cdn3.iconfinder.com/data/icons/fruits-52/150/icon_fruit_ameixa-512.png" />
-                <input type="checkbox" name="plum" />
+                <input type="checkbox" name="plum" onChange={this.handleInputChange}/>
               </label>
               <label name="pear">
                 <img src="https://cdn1.iconfinder.com/data/icons/fresh-fruit-2/128/pear-512.png" />
-                <input type="checkbox" name="pear" />
+                <input type="checkbox" name="pear" onChange={this.handleInputChange}/>
               </label>
               <label name="blueberries">
                 <img src="https://cdn3.iconfinder.com/data/icons/spring-2-1/30/Black_Berries-512.png" />
-                <input type="checkbox" name="blueberries" />
+                <input type="checkbox" name="blueberries" onChange={this.handleInputChange}/>
               </label>
             </div>
             <div className="BoardGeneratorForm-vegetableContainer">
               <label name="broccoli">
                 <img src="https://cdn4.iconfinder.com/data/icons/breakfast-14/48/broccoli-512.png" />
-                <input type="checkbox" name="broccoli" />
+                <input type="checkbox" name="broccoli" onChange={this.handleInputChange}/>
               </label>
               <label name="tomato">
                 <img src="https://cdn3.iconfinder.com/data/icons/veggies/512/tomato.png" />
-                <input type="checkbox" name="tomato" />
+                <input type="checkbox" name="tomato" onChange={this.handleInputChange}/>
               </label>
               <label name="carrot">
                 <img src="https://cdn3.iconfinder.com/data/icons/spring-23/32/carrot-vegetable-spring-food-512.png" />
-                <input type="checkbox" name="carrot" />
+                <input type="checkbox" name="carrot" onChange={this.handleInputChange}/>
               </label>
             </div>
             <div className="BoardGeneratorForm-selectAll">
-              <label name="allFriuits">
+              <label name="checkAllFriuits">
                 <span>Zaznacz wszystko</span>
-                <input type="checkbox" name="allFruits" />
+                <input type="checkbox" name="checkAllFriuits" />
+              </label>
+              <label name="uncheckAllFriuits">
+                <span>Odznacz wszystko</span>
+                <input type="checkbox" name="uncheckAllFriuits" />
               </label>
             </div>
           </div>
