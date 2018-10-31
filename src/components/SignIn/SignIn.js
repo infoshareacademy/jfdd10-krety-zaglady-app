d0235e53ea2eb153bf20e443bf12cd0d6534690b
Monika Jewula <mp.jewula@gmail.com>
2018-10-31 15:54:49 +0100

    signing in is done

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -3,7 +3,6 @@ import firebase from "firebase";
 
 import "./SignIn.css";
 
-
 class SignIn extends Component {
   state = {
     email: "",
@@ -14,20 +13,17 @@ class SignIn extends Component {
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
-    })
-  }
+    });
+  };
 
   handleSubmit = event => {
-    event.preventDefault()
-    firebase.auth().signInWithEmailAndPassword(
-      this.state.email,
-      this.state.password
-    ).then(
-      () => this.setState({ error: null })
-    ).catch(
-      error => this.setState({ error })
-    )
-  }
+    event.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => this.setState({ error: null }))
+      .catch(error => this.setState({ error }));
+  };
 
   render() {
     return (
@@ -37,32 +33,33 @@ class SignIn extends Component {
           <tbody>
             <tr>
               <td>
-        <input
-          placeholder="Adres e-mail"
-          name="email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
-        </td>
-        <td>
-        <input
-          placeholder="Hasło"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-          type="password"
-        />
-</td>
-<td>
-        <button id="sign-in" className="sign-in">Zaloguj się</button>
-        </td>
-        </tr>
-        </tbody>
-      </table>
+                <input
+                  placeholder="Adres e-mail"
+                  name="email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </td>
+              <td>
+                <input
+                  placeholder="Hasło"
+                  name="password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  type="password"
+                />
+              </td>
+              <td>
+                <button id="sign-in" className="sign-in">
+                  Zaloguj się
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </form>
     );
   }
 }
 
 export default SignIn;
-
