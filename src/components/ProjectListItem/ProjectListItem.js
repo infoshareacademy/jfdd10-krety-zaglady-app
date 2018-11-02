1102de848af3129f640c124d10e12bdb9189a1df
Balbuhhha <balbuhhha@gmail.com>
2018-11-02 14:34:52 +0100

    last fix with empty fruits object

--- src/components/ProjectListItem/ProjectListItem.js
+++ src/components/ProjectListItem/ProjectListItem.js
@@ -3,7 +3,7 @@ import ProjectAuthor from "../ProjectAuthor/ProjectAuthor";
 import "./ProjectListItem.css";
 import { Link } from "react-router-dom";
 import posed from "react-pose";
-import ProjectBoard from '../ProjectBoard/ProjectBoard'
+import ProjectBoard from "../ProjectBoard/ProjectBoard";
 import { Z_FILTERED } from "zlib";
 
 const PushUp = posed.div({
@@ -13,6 +13,7 @@ const PushUp = posed.div({
 class ProjectListItem extends Component {
   state = { hovering: false };
   render() {
+    console.log(this.props.fruits)
     return (
       <div className="ProjectListItem-container">
         <ProjectAuthor {...this.props.author} />
@@ -29,15 +30,22 @@ class ProjectListItem extends Component {
             className="ProjectListItem"
           >
             <div className="ProjectListItem-fruitIcons">
-              {Object.entries(this.props.fruits || {}).map(([key, value]) => {
+              {!this.props.fruits && <p>brak upraw</p>}
+              {Object.entries(this.props.fruits || {})
+                .map(([key, value]) => {
+                  return { key, value };
+                })
 
-                return {key, value}})
-              
-             .slice(0,3).map(fruit => {
-               return(<div className={fruit.key} ></div>);
-              })}
+                .slice(0, 3)
+                .map(fruit => {
+                  return <div className={fruit.key} />;
+                })}
             </div>
-                <img className="BoardImage" src={this.props.boardImage} alt="boardImage" />
+            <img
+              className="BoardImage"
+              src={this.props.boardImage}
+              alt="boardImage"
+            />
           </Link>
         </PushUp>
       </div>
@@ -45,6 +53,5 @@ class ProjectListItem extends Component {
   }
 }
 export default ProjectListItem;
-
 
 // alt={fruit.key}
